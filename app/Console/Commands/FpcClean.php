ef18d154fe6d1e4930bd74e65332ac06bc4eb7f0
Vladislav Shcheglov <vladislav@scheglov.org>
2023-11-25 11:16:18 +0800

    Match command name with command class

--- app/Console/Commands/FpcClean.php
+++ app/Console/Commands/FpcClean.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Cache;
 
-class FpcClear extends Command
+class FpcClean extends Command
 {
     /**
      * The name and signature of the console command.
